22b3e3fd36f337f1eca436331415db6f502a57c7
rafapatekoski <insightfulpate@gmail.com>
2024-01-02 10:24:13 -0300

    AT1

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 import streamlit as st
+import pandas as pd
+dados = {
+    'Nome': ['João', 'Maria', 'Carlos', 'Ana'],
+    'Idade': [25, 30, 22, 28],
+    'Salário': [5000, 6000, 4500, 5500]
+}
 
-st.write("Ta no ar")
+# Criando o DataFrame
+df = pd.DataFrame(dados)
+
+st.write(df)
+st.write("Atualização 1")
